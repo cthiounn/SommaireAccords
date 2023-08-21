b87edf8db8da5351cbc846a10a402365a5e40570
cthiounn2 <thiounn.conrad@gmail.com>
2023-08-21 13:19:18 +0000

    api++

--- streamlit-api.py
+++ streamlit-api.py
@@ -11,6 +11,14 @@ TESSERACT_LANGUAGE="fra"
 SHOW_IMAGE=True
 PRINT_DETECTED_TITLE=False
 
+
+modele = st.radio(
+    "Modèle d'inférence de la Dares",
+    ('Modèle 1 : 7000', 'Modèle 2 : 900'))
+base_threshold = st.slider('Base Threshold', 0, 100, 50) / 100
+modele_threshold = st.slider('Dares Threshold', 0, 100, 50) / 100
+
+
 def pdf_to_img(pdf_file):
     return pdf2image.convert_from_path(
         pdf_file, grayscale=False, thread_count=os.cpu_count()
@@ -19,28 +27,25 @@ def pdf_to_img(pdf_file):
 model = lp.models.Detectron2LayoutModel(
     config_path ="config.yaml",
     model_path ="model_final.pth",
-    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", base_threshold],
     label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"},
 )
 
 model1 = lp.models.Detectron2LayoutModel(
     config_path ="config_dares.yaml",
     model_path ="model_final_dares_1.pth",
-    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", modele_threshold],
     label_map={3: "Text", 4: "Title", 1: "List", 2: "Table", 0: "Figure"},
 )
 
 model2 = lp.models.Detectron2LayoutModel(
     config_path ="config_dares.yaml",
     model_path ="model_final_dares_2.pth",
-    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+    extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", modele_threshold],
     label_map={3: "Text", 4: "Title", 1: "List", 2: "Table", 0: "Figure"},
 )
-
-
-col1, col2, col3 = st.columns(3)
-
 uploaded_file = st.file_uploader("Choisir un PDF")
+col1, col2 = st.columns(2)
 if uploaded_file is not None:
     file_name="tempfilename"
     temp_local_dir = tempfile.mkdtemp()
@@ -60,9 +65,10 @@ if uploaded_file is not None:
             with col1:
                 st.image(lp.draw_box(one_image_np, layout, box_width=3, show_element_type=True), caption="Titres détectés", use_column_width=True)
             with col2:
-                st.image(lp.draw_box(one_image_np, layout1, box_width=3, show_element_type=True), caption="Titres détectés", use_column_width=True)
-            with col3:
-                st.image(lp.draw_box(one_image_np, layout2, box_width=3, show_element_type=True), caption="Titres détectés", use_column_width=True)
+                if modele=='Modèle 1 : 7000':
+                    st.image(lp.draw_box(one_image_np, layout1, box_width=3, show_element_type=True), caption="Titres détectés", use_column_width=True)
+                else:
+                    st.image(lp.draw_box(one_image_np, layout2, box_width=3, show_element_type=True), caption="Titres détectés", use_column_width=True)
         title_blocks = lp.Layout([b for b in layout if b.type == "Title"])
         ocr_agent = lp.TesseractAgent(languages=TESSERACT_LANGUAGE)
         for block in title_blocks:
